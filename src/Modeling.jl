82482fef68f1389edb78fcf9d37972a8741400b1
Isaac Smith <smithisaacr@gmail.com>
2022-02-02 17:01:28 -0700

    Adding Eulers and RK4 methods

--- src/Modeling.jl
+++ src/Modeling.jl
@@ -1,6 +1,6 @@
 module Modeling
 
-export StateSpace, DiscreteSS
+export StateSpace, DiscreteSS, rk4, eulers, next_x
 
 using LinearAlgebra
 
@@ -26,6 +26,34 @@ mutable struct DiscreteSS
 	this.C = ss.C
 	return this
     end
+end
+
+function next_x(dss::DiscreteSS, x::Vector{Float64}, u::Float64)
+    return dss.Ad * x + dss.Bd * u
+end
+
+function rk4(f::Function, x::Vector{Float64}, dt::Float64, params, u::Float64)
+    k1 = f(x, params, u)
+    k2 = f(x + dt * k1 / 2, params, u)
+    k3 = f(x + dt * k2 / 2, params, u)
+    k4 = f(x + dt * k3, params, u)
+    return x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+end
+
+function rk4(ss::StateSpace, x::Vector{Float64}, dt::Float64, u::Float64)
+    k1 = ss.A * x + ss.B * u
+    k2 = ss.A * (x + dt * k1 / 2) + ss.B * u
+    k3 = ss.A * (x + dt * k2 / 2) + ss.B * u
+    k4 = ss.A * (x + dt * k3) + ss.B * u
+    return x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+end
+
+function eulers(f::Function, x::Vector{Float64}, dt::Float64, params, u::Float64)
+    return f(x, params, u) * dt + x
+end
+
+function eulers(ss::StateSpace, x::Vector{Float64}, dt::Float64, u::Float64)
+    return (ss.A * x + ss.B * u) * dt + x
 end
 
 end # module
