9eec59f81bc15a967d4c807acd5aa3a0cf6ae519
Isaac Smith <smithisaacr@gmail.com>
2022-06-21 11:46:45 -0600

    Changing Float64 to Real

--- src/Modeling.jl
+++ src/Modeling.jl
@@ -5,22 +5,22 @@ export StateSpace, DiscreteSS, rk4, eulers, next_x, get_y
 using LinearAlgebra
 
 struct StateSpace
-    A::Matrix{Float64}
-    B::Vector{Float64}
-    C::Matrix{Float64}
-    D::Float64
+    A::Matrix{<:Real}
+    B::Vector{<:Real}
+    C::Matrix{<:Real}
+    D::Real
 
-    function StateSpace(a::Matrix, b::Vector, c::Matrix, d::Float64 = 0.)
+    function StateSpace(a::Matrix{<:Real}, b::Vector{<:Real}, c::Matrix{<:Real}, d::Real = 0)
 	return new(a, b, c, d)
     end
 
     # Creates state space from float values
-    function StateSpace(a::Float64, b::Float64, c::Float64, d::Float64 = 0.)
+    function StateSpace(a::Real, b::Real, c::Real, d::Real = 0.)
 	return new(fill(a, (1,1)), [b], fill(c, (1, 1)), d)
     end
 end
 
-function msd(;m::Float64 = 1., b::Float64 = 1., k::Float64 = 1.)
+function msd(;m::Real = 1., b::Real = 1., k::Real = 1.)
     A = [
 	 0 1;
 	 (-k/m) (-b/m)
@@ -30,21 +30,21 @@ end
 
 # Maybe add low- and high-pass filters in here so that I can easily define them. 
 
-function low_pass(wc::Float64)
+function low_pass(wc::Real)
     return StateSpace(-wc, 1., wc)
 end
 
-function high_pass(wc::Float64)
+function high_pass(wc::Real)
     return StateSpace(-wc, 1., -wc, 1.)
 end
 
 mutable struct DiscreteSS
-    Ad::Matrix{Float64}
-    Bd::Vector{Float64}
-    C::Matrix{Float64}
-    D::Float64
+    Ad::Matrix{<:Real}
+    Bd::Vector{<:Real}
+    C::Matrix{<:Real}
+    D::Real
 
-    function DiscreteSS(ss::StateSpace, dt::Float64)
+    function DiscreteSS(ss::StateSpace, dt::Real)
 	this = new()
 	this.Ad = exp(ss.A * dt)
 	if det(ss.A) == 0
@@ -58,19 +58,19 @@ mutable struct DiscreteSS
     end
 end
 
-function get_y(ss::StateSpace, x::Vector{Float64}, u::Float64 = 0.)
+function get_y(ss::StateSpace, x::Vector{<:Real}, u::Real = 0.)
     return (ss.C * x)[1, 1] + ss.D * u
 end
 
-function get_y(dss::DiscreteSS, x::Vector{Float64}, u::Float64 = 0.)
+function get_y(dss::DiscreteSS, x::Vector{<:Real}, u::Real = 0.)
     return (dss.C * x)[1, 1] + dss.D * u
 end
 
-function next_x(dss::DiscreteSS, x::Vector{Float64}, u::Float64 = 0.)
+function next_x(dss::DiscreteSS, x::Vector{<:Real}, u::Real = 0.)
     return dss.Ad * x + dss.Bd * u
 end
 
-function rk4(f::Function, x::Vector{Float64}, dt::Float64, params, u::Float64 = 0.)
+function rk4(f::Function, x::Vector{<:Real}, dt::Real, params, u::Real = 0.)
     k1 = f(x, params, u)
     k2 = f(x + dt * k1 / 2, params, u)
     k3 = f(x + dt * k2 / 2, params, u)
@@ -78,7 +78,7 @@ function rk4(f::Function, x::Vector{Float64}, dt::Float64, params, u::Float64 = 
     return x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 end
 
-function rk4(ss::StateSpace, x::Vector{Float64}, dt::Float64, u::Float64 = 0.)
+function rk4(ss::StateSpace, x::Vector{<:Real}, dt::Real, u::Real = 0.)
     k1 = ss.A * x + ss.B * u
     k2 = ss.A * (x + dt * k1 / 2) + ss.B * u
     k3 = ss.A * (x + dt * k2 / 2) + ss.B * u
@@ -86,11 +86,11 @@ function rk4(ss::StateSpace, x::Vector{Float64}, dt::Float64, u::Float64 = 0.)
     return x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 end
 
-function eulers(f::Function, x::Vector{Float64}, dt::Float64, params, u::Float64 = 0.)
+function eulers(f::Function, x::Vector{<:Real}, dt::Real, params, u::Real = 0.)
     return f(x, params, u) * dt + x
 end
 
-function eulers(ss::StateSpace, x::Vector{Float64}, dt::Float64, u::Float64 = 0.)
+function eulers(ss::StateSpace, x::Vector{<:Real}, dt::Real, u::Real = 0.)
     return (ss.A * x + ss.B * u) * dt + x
 end
 
